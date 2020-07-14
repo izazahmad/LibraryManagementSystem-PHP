abe52c30f43ffd48658b040b28c244d585900b67
izazahmad <shekhizaz@gmail.com>
2020-07-14 07:17:58 +0100

    Initial commit

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<footer>
+   
+    <p>
+        Email: php.online@library.com<br>
+        Mobile: +116*******
+    </p>
+</footer>
+</body>
+</html>
